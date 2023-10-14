a64628d648b856a5908c6e537c5ef747dd955663
Jnail <jnail2018@alu.uct.cl>
2023-10-14 16:46:48 -0300

    test leer y subir datos a la bd
    aquí se subió una mejora en el código con la prueba para poder subir datos y leerlos desde la base de datos

--- pawpatitas/lib/animal_upload.dart
+++ pawpatitas/lib/animal_upload.dart
@@ -1,129 +1,105 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:image_picker/image_picker.dart';
-import 'dart:io';
 
-class AnimalUploadForm extends StatefulWidget {
-  @override
-  _AnimalUploadFormState createState() => _AnimalUploadFormState();
-}
-
-class _AnimalUploadFormState extends State<AnimalUploadForm> {
-  File? _image;
-  final picker = ImagePicker();
-  String? _animalName;
-  String? _animalBreed;
-  String? _animalDescription;
-  String? _animalIngressDate;
-
-  Future getImage() async {
-    final pickedFile = await picker.getImage(source: ImageSource.gallery);
-
-    if (pickedFile != null) {
-      setState(() {
-        _image = File(pickedFile.path);
-      });
-    }
-  }
-
+class TestPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Cargar Animal'),
+        title: Text('Datos de Firestore'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: FirestoreData(),
+    );
+  }
+}
+
+class FirestoreData extends StatefulWidget {
+  @override
+  _FirestoreDataState createState() => _FirestoreDataState();
+}
+
+class _FirestoreDataState extends State<FirestoreData> {
+  final TextEditingController nameController = TextEditingController();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+
+  void saveNameToFirestore() async {
+    final name = nameController.text;
+    if (name.isNotEmpty) {
+      try {
+        await FirebaseFirestore.instance.collection('people').add({'name': name});
+        nameController.clear(); // esto es para poder limpiar el formulario luego de escribir el nombre
+        _showSnackBar('Guardado exitosamente');
+      } catch (e) {
+        print('Error al guardar en Firestore: $e');
+      }
+    }
+  }
+
+ void _showSnackBar(String message) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(content: Text(message))
+  );
+}
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      body: SingleChildScrollView(
         child: Column(
           children: [
-            ElevatedButton(
-              onPressed: getImage,
-              child: Text('Seleccionar Imagen'),
-            ),
-            _image == null
-                ? Text('No se ha seleccionado ninguna imagen')
-                : Image.file(_image!),
-            TextField(
-              onChanged: (value) => _animalName = value,
-              decoration: InputDecoration(labelText: 'Nombre del Animal'),
-            ),
-            TextField(
-              onChanged: (value) => _animalBreed = value,
-              decoration: InputDecoration(labelText: 'Raza del Animal'),
-            ),
-            TextField(
-              onChanged: (value) => _animalDescription = value,
-              decoration: InputDecoration(labelText: 'Descripción del Animal'),
-            ),
-            TextField(
-              onChanged: (value) => _animalIngressDate = value,
-              decoration: InputDecoration(labelText: 'Fecha de Ingreso'),
+            // en esta parte esta el formulario
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextFormField(
+                controller: nameController,
+                decoration: InputDecoration(labelText: 'Nombre'),
+              ),
             ),
             ElevatedButton(
-              onPressed: () => uploadAnimal(),
-              child: Text('Añadir Mascota'),
+              onPressed: saveNameToFirestore,
+              child: Text('Guardar Nombre en Firestore'),
+            ),
+            StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance.collection('people').snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text('Error: ${snapshot.error}'),
+                  );
+                }
+
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                final docs = snapshot.data?.docs;
+
+                if (docs == null || docs.isEmpty) {
+                  return Center(
+                    child: Text('No hay datos disponibles'),
+                  );
+                }
+
+                return ListView(
+                  shrinkWrap: true,
+                  children: docs.map((DocumentSnapshot document) {
+                    final data = document.data() as Map<String, dynamic>;
+                    final name = data['name'] ?? 'Nombre no disponible';
+                    return ListTile(
+                      title: Text(name),
+                    );
+                  }).toList(),
+                );
+              },
             ),
           ],
         ),
       ),
     );
   }
-
-  Future<void> uploadAnimal() async {
-    try {
-      if (_image != null &&
-          _animalName != null &&
-          _animalBreed != null &&
-          _animalDescription != null &&
-          _animalIngressDate != null) {
-        final storage = FirebaseStorage.instance;
-        final storageRef = storage.ref().child('animal_images/${DateTime.now()}.png');
-        await storageRef.putFile(_image!);
-
-        final downloadUrl = await storageRef.getDownloadURL();
-
-        final firestore = FirebaseFirestore.instance;
-        firestore.collection('animals').add({
-          'name': _animalName,
-          'breed': _animalBreed,
-          'description': _animalDescription,
-          'ingressDate': _animalIngressDate,
-          'imageUrl': downloadUrl,
-        });
-
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text('Animal subido con éxito'),
-          ),
-        );
-
-        setState(() {
-          _image = null;
-          _animalName = null;
-          _animalBreed = null;
-          _animalDescription = null;
-          _animalIngressDate = null;
-        });
-      } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text('Por favor, complete todos los campos y seleccione una imagen'),
-          ),
-        );
-      }
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Error al subir el animal: $e'),
-        ),
-      );
-    }
-  }
-}
-
-void main() {
-  runApp(MaterialApp(
-    home: AnimalUploadForm(),
-  ));
 }
--- pawpatitas/lib/home.dart
+++ pawpatitas/lib/home.dart
@@ -22,7 +22,7 @@ class _HomePageState extends State<HomePage> {
     SoportePage(),
     DonarPage(),    
     PaginaMetricas(),
-    AnimalUploadForm(),
+    TestPage(),
   
   ];
 
